acfbc0f476d2ddd7dbb6207aadee64419e09205d
Varadraj Jagdale <144721104+Varadrajj@users.noreply.github.com>
2023-10-04 16:44:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-var express=require('express');
-var app=express();
-app.use(express.static('public'))
-app.get("/",(req, res)=>{res.send("Welcome to  IET");});
-app.get("/product",(req, res)=>{var p={ "id":45, "title":"Gerbera","description":"Wedding Flower", "unitprice":14, "quatity":7000 };
-                                       res.send(p);
-});
+var express = require('express');
+
+var app = express();
+
+//first o/p display
+app.get("/",(req,res)=>
+{
+  res.send("Welcome to IET");
+}
+);
+
+//second o/p display using localhost:8000/product
+app.get("/product",(req,res)=>
+  {
+     var p = {
+                "id" : 45,
+                "title" : "Gerbera",
+                "description" : "Wedding flower",
+                "Unit price" : 15,
+                "quantity" : 5000
+             };
+  
+             res.send(p);
+  }
+);
+
+//third o/p display linking or add all static html pages
+app.use(express.static('public'));
+
+
 app.listen(8000);
 console.log("App is listening on port 8000");
